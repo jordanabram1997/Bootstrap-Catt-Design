b878d7c26233dbff89fbf16d13079367409ce361
Jordan Abram <jordan16_abram@gmail.com>
2014-08-18 13:30:57 +0100

    added a Navigation Bar to all pages

--- js/main.js
+++ js/main.js
@@ -1,22 +1,8 @@
 $(document).ready(function() {
-    var owl = $("#owl-demo");
-
-    owl.owlCarousel({
-
+    $("#owl-demo").owlCarousel({
         autoPlay: 3000, //Set AutoPlay to 3 seconds
-
         items: 4,
         itemsDesktop: [1199, 3],
         itemsDesktopSmall: [979, 3]
-
     });
-
-    // Custom Navigation Events
-    $(".js-next").click(function() {
-        owl.trigger('owl.next');
-    })
-    $(".js-prev").click(function() {
-        owl.trigger('owl.prev');
-    })
-
 });
